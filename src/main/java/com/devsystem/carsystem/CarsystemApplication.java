844ff4f815fda30baedede6d13ce1828807b1d7c
T-Pontes <tiagopontes10@gmail.com>
2022-09-30 22:41:53 -0300

    Criacao do projeto

---
+++ src/main/java/com/devsystem/carsystem/CarsystemApplication.java
@@ -0,0 +1,13 @@
+package com.devsystem.carsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsystemApplication.class, args);
+	}
+
+}
